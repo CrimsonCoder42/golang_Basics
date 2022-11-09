1722978718652d66a1a22edf54be178dc9d32f8e
CrimsonCoder42 <nostro37@gmail.com>
2022-11-09 08:49:00 -0500

    dice game done

--- src/lectures/exercise/sr-dice/dice.go
+++ src/lectures/exercise/sr-dice/dice.go
@@ -23,7 +23,59 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func rollDice(sides int) int {
+	rand.Seed(time.Now().UnixNano())
+	randNum := rand.Intn(sides) + 1
+	return randNum
+}
+
+func multiRolls(roll, sides int) int {
+	count := 0
+	for i := 0; i < roll; i++ {
+		randNum := rollDice(sides)
+		count += randNum
+	}
+	return count
+}
+
+func snakeEyes(roll, dice int) bool {
+	if roll == 2 && dice == 2 {
+		return true
+	}
+	return false
+}
+
+func isEven(num int) bool {
+	if num%2 == 0 {
+		return true
+	}
+	return false
+}
 
 func main() {
+
+	numOfSides := 6
+	numOfDice := 2
+	numOfRolls := 2
+	totalRolls := numOfDice * numOfRolls
+
+	rollTotal := multiRolls(totalRolls, numOfSides)
+	//* Print the sum of the dice roll
+	if snakeEyes(rollTotal, numOfDice) {
+		fmt.Println("Snake eyes")
+	} else if rollTotal == 7 {
+		fmt.Println("Lucky 7", rollTotal)
+	} else if isEven(rollTotal) {
+		fmt.Println("Even", rollTotal)
+	} else if !isEven(rollTotal) {
+		fmt.Println("Odd", rollTotal)
+	} else {
+		fmt.Println(rollTotal)
+	}
 }
